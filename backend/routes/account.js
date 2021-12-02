6be20f8f54cb9208bc94bdc6c75b110429ca0a46
TranTanMinh <myemail>
2021-12-02 10:25:36 +0700

    edit account post routes

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import config from '../config.js';
+import bodyParser from 'body-parser';
 import pkg from 'pg';
 
 const {Pool} = pkg;
@@ -7,11 +8,12 @@ const pool = new Pool(config.POSTGRES_INFO)
 
 const router = express.Router();
 const app = express()
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}))
 
 router.get('/', async (req, res) => {
     try {
         var accounts = await pool.query('SELECT * FROM account')
+        console.log('get')
     } catch (err) {
         console.log(err.stack)
     }
@@ -19,22 +21,21 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    const text = 'INSERT INTO account(account_id, username, current_password, is_active, user_role, full_name, birth_date, email, phone_number, avatar, last_updated_stamp, created_stamp) \
-                                VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *'
-    // const values = ['M001', 'minhtt', '161718', true, 'admin', 'Tan Minh Tran', '08-16-2000', 'tanminhtran168@gmail.com', '123456789', 'avatar.png', NULL, CURRENT_TIMESTAMP]
-    console.log('==============================================================')
-    console.log(req.body)
-    // try {
-    //     var account = await pool.query(text)
-    // } catch (err) {
-    //     console.log(err.stack)
-    // }
-    // if (account) {
-    //     return res.status(201).send({message: 'new account created', data: account.rows});
-    // } else {
-    //     return res.status(500).send({message: 'error in creating new account'});
-    // }
-    res.send('no')
+    console.log('post')
+    const text = 'INSERT INTO account(account_id, username, current_password, is_active, user_role, full_name, birth_date, email, phone_number, avatar, last_updated_stamp) \
+                                 VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *'
+    const values = ['M003', 'minhtt', '161718', true, 'admin', 'Tan Minh Tran', '08-16-2000', 'tanminhtran168@gmail.com', '123456789', 'avatar.png', null]
+    console.log(req)
+    try {
+        var account = await pool.query(text, values)
+    } catch (err) {
+        console.log(err.stack)
+    }
+    if (account) {
+        return res.status(201).send({message: 'new account created', data: account.rows});
+    } else {
+        return res.status(500).send({message: 'error in creating new account'});
+    }
 })
 
 export default router;
