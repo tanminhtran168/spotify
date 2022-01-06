6b66e35aac95b4702fd5570ec3888594f7c08ffd
ngonamduonghl <duongnamngohl@gmail.com>
2022-01-06 22:50:31 +0700

    update_db

--- spotify-script.sql
+++ spotify-script.sql
@@ -2,6 +2,7 @@ CREATE TABLE account (
   account_id SERIAL NOT NULL,
   username VARCHAR(60),
   current_password VARCHAR(60),
+  avatar varchar(60),
   user_role VARCHAR(60),
   full_name VARCHAR(60),
   birth_date DATE,
@@ -26,24 +27,24 @@ CREATE TABLE artist (
   artist_id serial NOT NULL,
   artist_name VARCHAR(60),
   artist_info VARCHAR(1000),
+  artist_image VARCHAR(100),
   birth_date DATE,
   num_of_songs INTEGER,
   last_updated_stamp TIMESTAMP NULL,
   created_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT pk_artist PRIMARY KEY (artist_id),
-  CONSTRAINT admin_creating FOREIGN KEY (admin_id) REFERENCES admin (admin_id)
+  CONSTRAINT pk_artist PRIMARY KEY (artist_id)
 );
 
 CREATE TABLE album (
   album_id serial NOT NULL,
   artist_id int NOT NULL,
   album_name VARCHAR(60),
+  album_image VARCHAR(100),
   num_of_songs INTEGER,
   total_duration INTEGER,
   last_updated_stamp TIMESTAMP NULL,
   created_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT pk_album PRIMARY KEY (album_id),
-  CONSTRAINT admin_creating FOREIGN KEY (admin_id) REFERENCES admin (admin_id),
   CONSTRAINT artist_composing FOREIGN KEY (artist_id) REFERENCES artist (artist_id)
 );
 
@@ -52,6 +53,7 @@ CREATE TABLE song (
   artist_id int NOT NULL,
   album_id int ,
   song_name VARCHAR(60),
+  song_image VARCHAR(100),
   duration INTEGER,
   category VARCHAR(60),
   average_rate NUMERIC,
@@ -87,8 +89,8 @@ CREATE TABLE comment (
 );
 
 CREATE TABLE artist_favorite (
-  client_id VARCHAR(60) NOT NULL,
-  artist_id VARCHAR(60) NOT NULL,
+  client_id int NOT NULL,
+  artist_id int NOT NULL,
   last_updated_stamp TIMESTAMP NULL,
   created_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT pk_client_artist PRIMARY KEY (client_id, artist_id),
@@ -117,3 +119,15 @@ CREATE table song_added_to_playlist (
   CONSTRAINT playlist_included FOREIGN KEY (playlist_id) REFERENCES playlist (playlist_id),
   CONSTRAINT song_added FOREIGN KEY (song_id) REFERENCES song (song_id)
 );
+
+INSERT INTO account(account_id, username, current_password, user_role, full_name, birth_date, email, phone_number, last_updated_stamp, created_stamp) 
+            VALUES(default, 'minhtt', '161718', 'admin', 'Tan Minh Tran', null, 'tanminhtran168@gmail.com', '123456789', null, default)
+INSERT INTO account(account_id, username, current_password, is_active, user_role, full_name, birth_date, email, phone_number, last_updated_stamp, created_stamp)
+            VALUES(default, 'duongnn', '123456', 'client', 'Nam Duong Ngo', null, 'duongnamngohl@gmail.com', '0963648035', null, default)
+select * from account 
+select * from artist  
+select * from song 
+SELECT * FROM account WHERE account_id = '1'
+
+drop table account, client, artist, album, song, song_added_to_playlist, playlist, comment, rating , artist_favorite 
+
