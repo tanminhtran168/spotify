c4b81b18ad3af3cd8c7326f98db23aa64f95e511
ngonamduonghl <duongnamngohl@gmail.com>
2022-02-20 22:13:23 +0700

    add function

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -23,3 +23,22 @@ export const get_song_manage = async (req, res) => {
     
     res.render('admin/manage-song', {layout: 'admin/layout'}) 
 }
+export const getNumSong = async (req, res) => {
+    const num = await pool.query('SELECT COUNT(song_id) as numsong FROM song')
+    res.send(num.rows[0].numsong)
+}
+
+export const getNumAlbum = async (req, res) => {
+    const num = await pool.query('SELECT COUNT(album_id) as numalbum FROM album')
+    res.send(num.rows[0].numalbum)
+}
+
+export const getNumArtist = async (req, res) => {
+    const num = await pool.query('SELECT COUNT(artist_id) as numartist FROM artist')
+    res.send(num.rows[0].numartist)
+}
+
+export const getNumClient = async (req, res) => {
+    const num = await pool.query('SELECT COUNT(client_id) as numclient FROM client')
+    res.send(num.rows[0].numclient)
+}
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser';
 import { checkAdmin, isAuth } from '../utils.js'
-import { get_homepage, get_song_manage } from '../controllers/adminController.js';
+import { get_homepage, get_song_manage} from '../controllers/adminController.js';
 const router = express.Router();
 
 router.use(bodyParser.json())
