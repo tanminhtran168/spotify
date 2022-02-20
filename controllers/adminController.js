d933fc4a307c3a1537fa3bd177fe2aae3d3dd158
finefinnfinning <finefinnfinning@gmail.com>
2022-02-20 22:20:47 +0700

    rebase-test

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,25 @@
 import express from 'express'
 import pg from 'pg'
-import jwt from 'jsonwebtoken'
 import config from '../config.js'
+import { convertIntToTimeString } from '../utils.js'
+const router = express.Router()
+const Pool = pg.Pool
+const pool = new Pool(config.POSTGRES_INFO)
 
 export const get_homepage = async (req, res) => {
     res.render('admin/homepage', {layout: 'admin/layout'}) 
+}
+
+export const get_song_manage = async (req, res) => {
+    try{
+        const songs = await pool.query('SELECT song.*, artist_name, album_name FROM song, artist, album WHERE song.artist_id = artist.artist_id and song.album_id = album.album_id')
+        songs.rows.forEach(row => {
+            row.duration = convertIntToTimeString(row.duration)
+        })
+        res.locals.songs = songs.rows
+    } catch (err) {
+        console.log(err.stack)
+    }
+    
+    res.render('admin/manage-song', {layout: 'admin/layout'}) 
 }
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser';
 import { checkAdmin, isAuth } from '../utils.js'
-import { get_homepage } from '../controllers/adminController.js';
+import { get_homepage, get_song_manage } from '../controllers/adminController.js';
 const router = express.Router();
 
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({extended:true}))
 
 router.get('/', get_homepage);
+router.get('/song', get_song_manage);
 
 export default router;
