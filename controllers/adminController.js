5baff99f25c04a4f425681a4772b6b49253317ca
ngonamduonghl <duongnamngohl@gmail.com>
2022-02-20 22:13:23 +0700

    add function

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,7 +2,29 @@ import express from 'express'
 import pg from 'pg'
 import jwt from 'jsonwebtoken'
 import config from '../config.js'
+const Pool = pg.Pool
+const pool = new Pool(config.POSTGRES_INFO)
 
 export const get_homepage = async (req, res) => {
     res.render('admin/homepage', {layout: 'admin/layout'}) 
+}
+
+export const getNumSong = async (req, res) => {
+    const num = await pool.query('SELECT COUNT(song_id) as numsong FROM song')
+    res.send(num.rows[0].numsong)
+}
+
+export const getNumAlbum = async (req, res) => {
+    const num = await pool.query('SELECT COUNT(album_id) as numalbum FROM album')
+    res.send(num.rows[0].numalbum)
+}
+
+export const getNumArtist = async (req, res) => {
+    const num = await pool.query('SELECT COUNT(artist_id) as numartist FROM artist')
+    res.send(num.rows[0].numartist)
+}
+
+export const getNumClient = async (req, res) => {
+    const num = await pool.query('SELECT COUNT(client_id) as numclient FROM client')
+    res.send(num.rows[0].numclient)
 }
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser';
 import { checkAdmin, isAuth } from '../utils.js'
-import { get_homepage } from '../controllers/adminController.js';
+import { get_homepage, getNumAlbum, getNumArtist, getNumSong, getNumClient} from '../controllers/adminController.js';
 const router = express.Router();
 
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({extended:true}))
 
 router.get('/', get_homepage);
+router.get('/getsong', getNumSong)
+router.get('/getalbum', getNumAlbum)
+router.get('/getartist', getNumArtist)
+router.get('/getclient', getNumClient)
 
 export default router;
